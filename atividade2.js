8ecbf54ab2cef6021c847278db8774b2fefd1b44
fej0se <80367187+fej0se@users.noreply.github.com>
2021-05-13 21:34:23 -0300

    Update atividade2.js

--- atividade2.js
+++ atividade2.js
@@ -1,58 +1,57 @@
 const listaProdutos = require('./listaProdutos');
 
 //Quantidade total de itens em estoque (somatória das quantidades de todos os produtos)
-function totalProductsQuantity(products) {
+function totalProducts(products) {
     let quant = 0;
     for (product of products) {
         quant += product.qtdEstoque;
     }
-    return quant;
+    console.log(`total de produtos: ${quant}`);
 }
 
-console.log(totalProductsQuantity(listaProdutos))
+totalProducts(listaProdutos);
 
 //Quantidade total de itens em destaque (somatória das quantidades dos itens marcados como "emDestaque : sim")
-function featuredProducts(products) {
+function totalFeaturedProducts(products) {
     let quant = 0;
     for (product of products) {
         if (product.emDestaque == "sim") {
             quant += product.qtdEstoque;
         }
     }
-    return quant;
+    console.log(`total de produtos em destaque: ${quant}`);
 }
 
-console.log(featuredProducts(listaProdutos))
+totalFeaturedProducts(listaProdutos);
 
 //Quantidade total de itens disponíveis (similar ao anterior)
-function productQuantityWithFilters(products) {
+function totalAvailableProducts(products) {
     let quant = 0;
     for (product of products) {
         if (product.disponivel == 'sim') {
             quant += product.qtdEstoque
         }
     }
-
-    return quant;
+    console.log(`total de produtos disponíveis: ${quant}`);
 }
 
-console.log(productQuantityWithFilters(listaProdutos))
+totalAvailableProducts(listaProdutos);
 
 //Valor total do inventário da empresa (somatória dos valores individuais multiplicado pela quantidade em estoque)
-function getPatrimony(products){
+function companyInventory(products){
     let total = 0
     products.forEach((product) => {
         if (product.disponivel == 'sim'){
         total += (product.preco * product.qtdEstoque)
         }
     })
-    return total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    console.log(`total do inventário: ${total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`) 
 }
 
-console.log(getPatrimony(listaProdutos))
+companyInventory(listaProdutos);
 
 //Somatória de itens por departamento (você deverá retornar um objeto contendo o nome do departamento e o total de itens nele)
-function totalQtdByDepartament(products) {
+function totalProductsByDepartament(products) {
     let deptos = [];
     for (let i = 0; i < products.length; i++) {
         let index = deptos.findIndex(val => val.departamento == products[i].departamento.nomeDepto)
@@ -62,13 +61,13 @@ function totalQtdByDepartament(products) {
             deptos[index].total += products[i].qtdEstoque
         }
     }
-    return deptos
+    console.log(deptos);
 }
 
-console.log(totalQtdByDepartament(listaProdutos))
+totalProductsByDepartament(listaProdutos)
 
 //Valor total do inventário por departamento (similar ao item anterior)
-function departamentPatrimony(products) {
+function departamentInventory(products) {
     let deptos = [];
     for (let i = 0; i < products.length; i++) {
         let index = deptos.findIndex(array => array.departamento == products[i].departamento.nomeDepto)
@@ -79,13 +78,13 @@ function departamentPatrimony(products) {
         }
     }
     for(depto of deptos) {
-        depto.inventario = depto.inventario.toFixed(2);
+        depto.inventario = depto.inventario.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 
     }
-    return deptos;
+    console.log(deptos);
 }
 
-console.log(departamentPatrimony(listaProdutos))
+departamentInventory(listaProdutos);
 
 //Valor do ticket médio dos produtos da empresa (basicamente o valor total do inventário dividido pelo número de itens)
 function ticket(products){
@@ -97,10 +96,10 @@ function ticket(products){
             quantItem += product.qtdEstoque
         }
     }
-    return (sum / quantItem).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});;
+    console.log(`ticket médio: ${(sum / quantItem).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`);
 }
 
-console.log(ticket(listaProdutos))
+ticket(listaProdutos)
 
 //Ticket médio por departamento (similar ao item anterior, porém retornando uma lista de objetos que contenha o nome do departamento e o seu ticket médio)
 function departamentTicket(products) {
@@ -115,30 +114,43 @@ function departamentTicket(products) {
         }
     }
     for(depto of deptos) {
-        depto.ticket = (depto.valueTotal / depto.quantTotal).toFixed(2);
+        depto.ticket = (depto.valueTotal / depto.quantTotal).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
         delete depto.valueTotal;
         delete depto.quantTotal
     }
-    return deptos;
+    console.log(deptos);
 }
 
-console.log(departamentTicket(listaProdutos))
+departamentTicket(listaProdutos);
 
 //Departamento mais valioso (qual o departamento que tem a maior somatória dos valores dos itens)
-function mostValuedDepartament(products) {
-    let deptos = departamentPatrimony(products);
+function mostValuableDepartament(products) {
+    let deptos = [];
+    for (let i = 0; i < products.length; i++) {
+        let index = deptos.findIndex(array => array.departamento == products[i].departamento.nomeDepto)
+        if (index < 0) {
+            deptos.push({ departamento: products[i].departamento.nomeDepto, inventario: (products[i].preco * products[i].qtdEstoque)})
+        } else {
+            deptos[index].inventario += (products[i].preco * products[i].qtdEstoque)
+        }
+    }
+    for(depto of deptos) {
+        depto.inventario = depto.inventario.toFixed(2);
+
+    }
+
     const mostValued = Math.max(...deptos.map(({ inventario }) => inventario))
     for (depto of deptos) {
         if (depto.inventario == mostValued) {
-            return `Departamento mais valioso: ${depto.departamento}`
+            console.log(`Departamento mais valioso: ${depto.departamento}`)
         }
     }
 }
 
-console.log(mostValuedDepartament(listaProdutos))
+mostValuableDepartament(listaProdutos);
 
 //Produto mais caro da loja (bem como seu departamento)
-function maxValue(products) {
+function highestValuedProduct(products) {
     const max = products.reduce((prodX, prodY) => {
         if (prodX.preco > prodY.preco) {
             return prodX;
@@ -146,13 +158,13 @@ function maxValue(products) {
             return prodY;
         }
     });
-    return `Produto mais caro da loja: ${max.descricao}, departamento: ${max.departamento.nomeDepto}`
+    console.log(`Produto mais caro da loja: ${max.descricao}, departamento: ${max.departamento.nomeDepto}`);
 }
 
-console.log(maxValue(listaProdutos))
+highestValuedProduct(listaProdutos);
 
 //Produto mais barato da loja (bem como seu departamento)
-function minValue(products) {
+function lowestValuedProduct(products) {
     const min = products.reduce((prodX, prodY) => {
         if (prodX.preco < prodY.preco) {
             return prodX;
@@ -160,7 +172,7 @@ function minValue(products) {
             return prodY;
         }
     })
-    return `Produto mais barato da loja: ${min.descricao}, departamento: ${min.departamento.nomeDepto}`
+    console.log(`Produto mais barato da loja: ${min.descricao}, departamento: ${min.departamento.nomeDepto}`);
 }
 
-console.log(minValue(listaProdutos))
+lowestValuedProduct(listaProdutos);
